d7d48b7132aaeac09b6f9d337797c194a8a1f279
PrzemyslawKulej <przemek.kulej1@gmail.com>
2023-07-10 22:44:18 +0200

    Finished login/register modal

--- js/main.js
+++ js/main.js
@@ -1,40 +1,37 @@
 const signupModal = document.getElementById('signup-modal');
-const loginModal = document.getElementById('login-modal');
+const wrapper = document.querySelector('.modal-container')
+const loginLink = document.querySelector('.login-link')
+const registerLink = document.querySelector('.register-link')
 
-// Get the button that opens the modals
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');
+});
+
+// Get the button that opens the modal
 const signupBtn = document.getElementById("signup-button");
-const loginBtn = document.getElementById("login-button");
 
-// Get the <span> element that closes the modals
-const span = document.getElementsByClassName("close-button");
+// Get the <span> element that closes the modal
+const closeButtons = document.getElementsByClassName("close-button");
 
 // When the user clicks the button, open the modal
 signupBtn.onclick = function() {
     signupModal.style.display = "block";
 }
 
-loginBtn.onclick = function() {
-    loginModal.style.display = "block";
-}
-
 // When the user clicks on <span> (x), close the modal
-for (let i = 0; i < span.length; i++) {
-    span[i].onclick = function() {
-        if(signupModal.style.display === "block") {
-            signupModal.style.display = "none";
-        }
-        if(loginModal.style.display === "block") {
-            loginModal.style.display = "none";
-        }
-    }
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target === signupModal) {
+for (let i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].onclick = function() {
         signupModal.style.display = "none";
     }
-    if (event.target === loginModal) {
-        loginModal.style.display = "none";
+}
+
+// Close the modal when user clicks outside of the form
+window.onclick = function(event) {
+    if (event.target == signupModal) {
+        signupModal.style.display = "none";
     }
 }
