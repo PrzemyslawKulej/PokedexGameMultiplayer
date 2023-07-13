37f8ec38fc1b1f92b216cae78518bb8aa663a1b2
PrzemyslawKulej <przemek.kulej1@gmail.com>
2023-07-13 18:30:38 +0200

    Connecting login/register form to database

--- server.js
+++ server.js
@@ -1,23 +1,46 @@
 const mongoose = require('mongoose').default;
 const express = require('express');
 const bcrypt = require('bcrypt');
+const router = express.Router();
+const path = require('path');
 const app = express();
 const port = 3000;
 
-mongoose.connect('mongodb://localhost:27017/myapp')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('Could not connect to MongoDB', err));
 
-app.get('/', (req, res) => {
-    res.send('Server is working!');
-});
 
 app.listen(port, () => {
     console.log(`Server listen at http://localhost:${port}`);
 });
 
+// MongoDB configuration
+
+mongoose.connect('mongodb://localhost:27017/myapp')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Could not connect to MongoDB', err));
 
 const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: { type: String, required: true },
+    mail: {type: String, required: true},
+    password: { type: String, required: true }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+// This line enables the parsing of request bodies as JSON
+app.use(express.json());
+
+// Setting EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+// To serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('main');
+});
 
 router.post('/register', async (req, res) => {
     // Check, if user exist
@@ -31,6 +54,7 @@ router.post('/register', async (req, res) => {
     // Create new user
     let user = new User({
         username: req.body.username,
+        mail: req.body.mail,
         password: hashedPassword
     });
     user = await user.save();
@@ -51,14 +75,12 @@ router.post('/login', async (req, res) => {
     res.send('Logged sucesfull!');
 });
 
+app.use('/api/users', router);
 
-const UserSchema = new Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true }
-});
 
-const User = mongoose.model('User', UserSchema);
 
-// comma
-// comma
+
+
+
+
 
